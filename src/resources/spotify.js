8bc7739b9a98efe3967ad9ac90f1a053ba87ab8a
Marcio Wolff <m.wolff.dos.santos@accenture.com>
2019-11-11 19:37:59 -0300

    ajusta servicos

--- src/resources/spotify.js
+++ src/resources/spotify.js
@@ -5,12 +5,12 @@ export const spotifyServiceFactory = ({ httpClient }) => ({
     return httpClient
       .get(`/search?q=${search}&type=artist,album,track`)
       .then(responseHandler)
-      .then(({ data }) => data);
+      .then(data => data);
   },
   albums(id) {
     return httpClient
       .get(`/albums/${id}`)
       .then(responseHandler)
-      .then(({ data }) => data);
+      .then(data => data);
   }
 });
